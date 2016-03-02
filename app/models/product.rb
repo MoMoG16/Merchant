310fd7d07c13c226190477574fa667ad789c872a
Moira Glace <mglace16@gmail.com>
2016-03-02 09:02:26 -0600

    Added models for cart and line items

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,9 @@ class Product < ActiveRecord::Base
   belongs_to :brand
   belongs_to :category
 
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_items
+
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ":style_missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
@@ -13,3 +16,18 @@ validates_numericality_of :current_price, greater_than_or_equal_to: 0.01
 validates_numericality_of :quantity, greater_than_or_equal_to: 0
 
 end
+
+private
+
+# TODO: make sure this works
+def ensure_not_referenced_by_any_line_items
+  if line_items.empty?
+    return true
+  else
+    errors.add(:base, "Cannote delete because line items are present")
+    return false
+  end
+
+end
+
+
